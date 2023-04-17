3c05401d4a649387dbd6d6d880f44b5460335848
Jakub Poleski <92997935+JacobzPL@users.noreply.github.com>
2023-04-17 17:33:55 +0200

    burger

--- src/template/top.php
+++ src/template/top.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SoundLab Studio</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+   <link href="../../assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
 
@@ -43,4 +44,5 @@
             </div>
         </div>
     </div>
+    <script src="../../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
 </header>
