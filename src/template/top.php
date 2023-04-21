3fd7c4e79af6184197219e5065e0dfeebec62a3c
Jakub Poleski <92997935+JacobzPL@users.noreply.github.com>
2023-04-21 18:55:56 +0200

    path 1.1.2

--- src/template/top.php
+++ src/template/top.php
@@ -27,7 +27,6 @@
   <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
     <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none">
     <img src="../../assets/images/logo.png" alt="" srcset="" class="bi me-2" width="100" height="100" role="img" aria-label="Bootstrap">
-    <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
     </a>
 
     <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
@@ -36,6 +35,16 @@
       <li><a href="#" class="nav-link px-2 link-body-emphasis">Customers</a></li>
       <li><a href="#" class="nav-link px-2 link-body-emphasis">Products</a></li>
     </ul>
+
+    
+    
+    <form class="d-flex" style='margin-right: 10px'>
+      <button class="btn btn-outline-dark" type="submit">
+          <i class="bi-cart-fill me-1"></i>
+          Koszyk 
+          <span class="badge bg-dark text-white ms-1 rounded-pill">10</span>
+      </button>
+    </form>
 
     <div class="dropdown text-end">
       <a href="#" class="d-block link-dark text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
@@ -47,7 +56,7 @@
         <li><a class="dropdown-item" href="#">Pomoc</a></li>
         <li><hr class="dropdown-divider"></li>
         <li><a class="dropdown-item" href="#">Wyloguj się</a></li>
-        </ul>
+      </ul>
     </div>
   </div>
 </div>
