c9b32aa6ce0a41a16973e3c4f05f1d71c0a2b9e6
Lisu <68614968+DevLisuu@users.noreply.github.com>
2023-04-23 10:59:45 +0200

    More products

--- src/template/kafelki.php
+++ src/template/kafelki.php
@@ -4,10 +4,10 @@
 
 			<div class="col mb-5">
 				<div class="card h-100">
-					<img class="card-img-top" src="../../assets/images/products/masno-gang.jpg" alt="...">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
 					<div class="card-body p-4">
 						<div class="text-center">
-							<h5 class="fw-bolder">Fancy Product</h5>$40.00 - $80.00
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
 						</div>
 					</div>
 					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
@@ -18,27 +18,88 @@
 
 			<div class="col mb-5">
 				<div class="card h-100">
-					<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>
-					<img class="card-img-top" src="../../assets/images/products/dycha.jpg" alt="...">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
 					<div class="card-body p-4">
 						<div class="text-center">
-							<h5 class="fw-bolder">Special Item</h5>
-							<div class="d-flex justify-content-center small text-warning mb-2">
-								<div class="bi-star-fill"></div>
-								<div class="bi-star-fill"></div>
-								<div class="bi-star-fill"></div>
-								<div class="bi-star-fill"></div>
-								<div class="bi-star-fill"></div>
-							</div>
-							<span class="text-muted text-decoration-line-through">$20.00</span> - $18.00
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
 						</div>
 					</div>
 					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
 						<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
 					</div>
-				<div>
+				</div>
 			</div>
-			
+
+			<div class="col mb-5">
+				<div class="card h-100">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
+					<div class="card-body p-4">
+						<div class="text-center">
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
+						</div>
+					</div>
+					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+						<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col mb-5">
+				<div class="card h-100">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
+					<div class="card-body p-4">
+						<div class="text-center">
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
+						</div>
+					</div>
+					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+						<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col mb-5">
+				<div class="card h-100">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
+					<div class="card-body p-4">
+						<div class="text-center">
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
+						</div>
+					</div>
+					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+						<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col mb-5">
+				<div class="card h-100">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
+					<div class="card-body p-4">
+						<div class="text-center">
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
+						</div>
+					</div>
+					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+						<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col mb-5">
+				<div class="card h-100">
+					<img class="card-img-top" src="../../assets/images/products/gang.jpg" alt="...">
+					<div class="card-body p-4">
+						<div class="text-center">
+							<h5 class="fw-bolder">Produkt 47</h5>47,47zł
+						</div>
+					</div>
+					<div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+						<div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Dodaj do koszyka</a></div>
+					</div>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </section>
