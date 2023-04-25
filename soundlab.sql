f2347af9c950ee026426a146f2987ea516656cac
Lisu <68614968+DevLisuu@users.noreply.github.com>
2023-04-25 14:55:46 +0200

    Remove unused data

--- soundlab.sql
+++ soundlab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 25, 2023 at 01:17 PM
+-- Generation Time: Apr 25, 2023 at 02:55 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -66,10 +66,7 @@ CREATE TABLE `produkty` (
   `id_produktu` int(11) NOT NULL,
   `tytul` varchar(255) DEFAULT NULL,
   `wykonawca` varchar(255) DEFAULT NULL,
-  `rok_wydania` int(11) DEFAULT NULL,
-  `gatunek` varchar(255) DEFAULT NULL,
   `cena` decimal(10,2) DEFAULT NULL,
-  `opis` text DEFAULT NULL,
   `zdjecie` varchar(255) DEFAULT NULL,
   `zdjecie2` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -78,17 +75,17 @@ CREATE TABLE `produkty` (
 -- Dumping data for table `produkty`
 --
 
-INSERT INTO `produkty` (`id_produktu`, `tytul`, `wykonawca`, `rok_wydania`, `gatunek`, `cena`, `opis`, `zdjecie`, `zdjecie2`) VALUES
-(1, 'GANG', 'Masno', NULL, NULL, 40.00, NULL, 'gang.jpg', 'gang2.jpg'),
-(2, 'Bufor (Standard)', 'Jan-rapowanie', NULL, NULL, 44.00, NULL, 'bufor_standard.jpg', 'bufor_standard2.jpg'),
-(3, 'Bufor (Vinyl)', 'Jan-rapowanie', NULL, NULL, 179.99, NULL, 'bufor_vinyl.jpg', 'bufor_vinyl.jpg'),
-(4, 'D jak DYCHA (mp3)', 'DYCHA & MASNO', NULL, NULL, 4.99, NULL, 'dycha.jpg', 'dycha2.jpg'),
-(5, 'KWIAT POLSKIEJ MŁODZIEŻY', 'Bedoes & Kubi Producent', NULL, NULL, 44.99, NULL, 'kwiat_pol.jpg', 'kwiat_pol1.jpg'),
-(6, 'PRETTY BOY (Standard)', 'White 2115', NULL, NULL, 44.99, NULL, 'prettyboy.jpg', 'prettyboy2.jpg'),
-(7, 'Evil_Things', 'Guzior', NULL, NULL, 33.99, NULL, 'evil-things.jpg', 'evil-things2.jpg'),
-(8, 'Pleśń', 'Guzior', NULL, NULL, 43.99, NULL, 'plesn.jpg', 'plesn2.jpg'),
-(9, '2.0', 'Chillwagon', NULL, NULL, 28.99, NULL, '2.0.jpg', '2.02.jpg'),
-(10, 'Jeszcze pięć minut', 'Kizo', NULL, NULL, 46.99, NULL, 'jeszczepiecminut.jpg', 'jeszczepiecminut2.jpg');
+INSERT INTO `produkty` (`id_produktu`, `tytul`, `wykonawca`, `cena`, `zdjecie`, `zdjecie2`) VALUES
+(1, 'GANG', 'Masno', 40.00, 'gang.jpg', 'gang2.jpg'),
+(2, 'Bufor (Standard)', 'Jan-rapowanie', 44.00, 'bufor_standard.jpg', 'bufor_standard2.jpg'),
+(3, 'Bufor (Vinyl)', 'Jan-rapowanie', 179.99, 'bufor_vinyl.jpg', 'bufor_standard2.jpg'),
+(4, 'D jak DYCHA (mp3)', 'DYCHA & MASNO', 4.99, 'dycha.jpg', 'dycha2.jpg'),
+(5, 'KWIAT POLSKIEJ MŁODZIEŻY', 'Bedoes & Kubi Producent', 44.99, 'kwiat_pol.jpg', 'kwiat_pol1.jpg'),
+(6, 'PRETTY BOY (Standard)', 'White 2115', 44.99, 'prettyboy.jpg', 'prettyboy2.jpg'),
+(7, 'Evil_Things', 'Guzior', 33.99, 'evil-things.jpg', 'evil-things2.jpg'),
+(8, 'Pleśń', 'Guzior', 43.99, 'plesn.jpg', 'plesn2.jpg'),
+(9, '2.0', 'Chillwagon', 28.99, '2.0.jpg', '2.02.jpg'),
+(10, 'Jeszcze pięć minut', 'Kizo', 46.99, 'jeszczepiecminut.jpg', 'jeszczepiecminut2.jpg');
 
 -- --------------------------------------------------------
 
@@ -103,6 +100,13 @@ CREATE TABLE `uzytkownicy` (
   `email` varchar(255) NOT NULL,
   `haslo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Dumping data for table `uzytkownicy`
+--
+
+INSERT INTO `uzytkownicy` (`id_klienta`, `imie`, `nazwisko`, `email`, `haslo`) VALUES
+(2, 'a', 'a', 'a@a', '12345678');
 
 --
 -- Indexes for dumped tables
@@ -158,7 +162,7 @@ ALTER TABLE `produkty`
 -- AUTO_INCREMENT for table `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
-  MODIFY `id_klienta` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_klienta` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
