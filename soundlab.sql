fd75e5990d527c476ecdd26c515f9e2165f71020
Lisu <68614968+DevLisuu@users.noreply.github.com>
2023-04-25 13:18:55 +0200

    Update db

--- soundlab.sql
+++ soundlab.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2023 at 04:33 PM
+-- Generation Time: Apr 25, 2023 at 01:17 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,29 +24,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `historie_zamowien`
+--
+
+CREATE TABLE `historie_zamowien` (
+  `id_zamowienia` int(11) NOT NULL,
+  `id_klienta` int(11) NOT NULL,
+  `id_produktu` int(11) NOT NULL,
+  `ilosc` int(11) NOT NULL,
+  `cena` decimal(10,2) NOT NULL,
+  `rodzaj_płatności` varchar(255) DEFAULT NULL,
+  `data_zamowienia` datetime NOT NULL,
+  `status` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klienci`
+--
+
+CREATE TABLE `klienci` (
+  `id_klienta` int(11) NOT NULL,
+  `imie` varchar(255) DEFAULT NULL,
+  `nazwisko` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `haslo` varchar(255) DEFAULT NULL,
+  `adres` varchar(255) DEFAULT NULL,
+  `miasto` varchar(255) DEFAULT NULL,
+  `kod_pocztowy` varchar(10) DEFAULT NULL,
+  `telefon` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `produkty`
 --
 
 CREATE TABLE `produkty` (
   `id_produktu` int(11) NOT NULL,
-  `tytul` varchar(255) NOT NULL,
-  `wykonawca` varchar(255) NOT NULL,
-  `cena` decimal(10,2) NOT NULL,
-  `zdjecie` varchar(255) NOT NULL,
-  `zdjecie2` varchar(255) NOT NULL
+  `tytul` varchar(255) DEFAULT NULL,
+  `wykonawca` varchar(255) DEFAULT NULL,
+  `rok_wydania` int(11) DEFAULT NULL,
+  `gatunek` varchar(255) DEFAULT NULL,
+  `cena` decimal(10,2) DEFAULT NULL,
+  `opis` text DEFAULT NULL,
+  `zdjecie` varchar(255) DEFAULT NULL,
+  `zdjecie2` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Dumping data for table `produkty`
 --
 
-INSERT INTO `produkty` (`id_produktu`, `tytul`, `wykonawca`, `cena`, `zdjecie`, `zdjecie2`) VALUES
-(1, 'GANG', 'Masno', 40.00, 'gang.jpg', 'gang2.jpg'),
-(2, 'Bufor (Standard)', 'Jan-rapowanie', 44.00, 'bufor_standard.jpg', 'bufor_standard.jpg'),
-(3, 'Bufor (Vinyl)', 'Jan-rapowanie', 179.99, 'bufor_vinyl.jpg', 'bufor_vinyl.jpg'),
-(4, 'D jak DYCHA (mp3)', 'DYCHA & MASNO', 4.99, 'dycha.jpg', 'dycha.jpg'),
-(5, 'KWIAT POLSKIEJ MŁODZIEŻY', 'Bedoes & Kubi Producent', 44.99, 'kwiat_pol.jpg', 'kwiat_pol.jpg'),
-(6, 'PRETTY BOY (Standard)', 'White 2115', 44.99, 'prettyboy.jpg', 'prettyboy2.jpg');
+INSERT INTO `produkty` (`id_produktu`, `tytul`, `wykonawca`, `rok_wydania`, `gatunek`, `cena`, `opis`, `zdjecie`, `zdjecie2`) VALUES
+(1, 'GANG', 'Masno', NULL, NULL, 40.00, NULL, 'gang.jpg', 'gang2.jpg'),
+(2, 'Bufor (Standard)', 'Jan-rapowanie', NULL, NULL, 44.00, NULL, 'bufor_standard.jpg', 'bufor_standard2.jpg'),
+(3, 'Bufor (Vinyl)', 'Jan-rapowanie', NULL, NULL, 179.99, NULL, 'bufor_vinyl.jpg', 'bufor_vinyl.jpg'),
+(4, 'D jak DYCHA (mp3)', 'DYCHA & MASNO', NULL, NULL, 4.99, NULL, 'dycha.jpg', 'dycha2.jpg'),
+(5, 'KWIAT POLSKIEJ MŁODZIEŻY', 'Bedoes & Kubi Producent', NULL, NULL, 44.99, NULL, 'kwiat_pol.jpg', 'kwiat_pol1.jpg'),
+(6, 'PRETTY BOY (Standard)', 'White 2115', NULL, NULL, 44.99, NULL, 'prettyboy.jpg', 'prettyboy2.jpg'),
+(7, 'Evil_Things', 'Guzior', NULL, NULL, 33.99, NULL, 'evil-things.jpg', 'evil-things2.jpg'),
+(8, 'Pleśń', 'Guzior', NULL, NULL, 43.99, NULL, 'plesn.jpg', 'plesn2.jpg'),
+(9, '2.0', 'Chillwagon', NULL, NULL, 28.99, NULL, '2.0.jpg', '2.02.jpg'),
+(10, 'Jeszcze pięć minut', 'Kizo', NULL, NULL, 46.99, NULL, 'jeszczepiecminut.jpg', 'jeszczepiecminut2.jpg');
 
 -- --------------------------------------------------------
 
@@ -59,27 +101,24 @@ CREATE TABLE `uzytkownicy` (
   `imie` varchar(255) NOT NULL,
   `nazwisko` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `haslo` varchar(255) NOT NULL,
-  `token` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `zamowienia`
---
-
-CREATE TABLE `zamowienia` (
-  `id_zamowienia` int(11) NOT NULL,
-  `id_klienta` int(11) NOT NULL,
-  `data_zamowienia` datetime NOT NULL,
-  `cena_calkowita` decimal(10,2) NOT NULL,
-  `status` varchar(20) NOT NULL
+  `haslo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `historie_zamowien`
+--
+ALTER TABLE `historie_zamowien`
+  ADD PRIMARY KEY (`id_zamowienia`);
+
+--
+-- Indexes for table `klienci`
+--
+ALTER TABLE `klienci`
+  ADD PRIMARY KEY (`id_klienta`);
 
 --
 -- Indexes for table `produkty`
@@ -94,43 +133,32 @@ ALTER TABLE `uzytkownicy`
   ADD PRIMARY KEY (`id_klienta`);
 
 --
--- Indexes for table `zamowienia`
---
-ALTER TABLE `zamowienia`
-  ADD PRIMARY KEY (`id_zamowienia`),
-  ADD KEY `id_klienta` (`id_klienta`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `historie_zamowien`
+--
+ALTER TABLE `historie_zamowien`
+  MODIFY `id_zamowienia` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `klienci`
+--
+ALTER TABLE `klienci`
+  MODIFY `id_klienta` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `produkty`
 --
 ALTER TABLE `produkty`
-  MODIFY `id_produktu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_produktu` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `uzytkownicy`
 --
 ALTER TABLE `uzytkownicy`
   MODIFY `id_klienta` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `zamowienia`
---
-ALTER TABLE `zamowienia`
-  MODIFY `id_zamowienia` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `zamowienia`
---
-ALTER TABLE `zamowienia`
-  ADD CONSTRAINT `zamowienia_ibfk_1` FOREIGN KEY (`id_klienta`) REFERENCES `uzytkownicy` (`id_klienta`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
